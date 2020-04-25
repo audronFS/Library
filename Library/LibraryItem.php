643769cb7f3e190f66aaf630c6761f4ec98332f9
Aurora del Rio <auroradelrio@gmail.com>
2020-04-25 20:11:28 +0100

    variable modify

--- Library/LibraryItem.php
+++ Library/LibraryItem.php
@@ -1,8 +1,42 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+Namespace Library;
 
+ //the books need to be selected only
+ //the books attributes
+
+ abstract class LibaryItem{
+ private $title;
+ private $category;
+ private $deweyDecimal;
+ 
+ public function __construct($title, $category, $deweyDecimal){
+ $this->title = $title;
+ $this->category = $category;
+ $this->deweyDecimal = $deweyDecimal;
+ }
+ 
+// getters only
+ public function getTitle(){
+ return $this->title;
+ }
+ 
+ public function getDewyDecimal(){
+ return $this->deweyDecimal; 
+ }
+ 
+ public function getCategory(){
+ return $this->category;
+ }
+ 
+ public function setTitle(){
+ $this->title=$title;
+ }
+ public function setDewyDecimal(){
+ $this->deweyDecimal=$deweyDecimal;
+ }
+ public function setCategory(){
+ $this->category=$category;
+ }
+
+ }
