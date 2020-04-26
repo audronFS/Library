853353af1b8ae18172ddfc873e14a5e2c04ba26c
Aurora del Rio <auroradelrio@gmail.com>
2020-04-26 11:16:13 +0100

    Adding first Branch Book.php

---
+++ Library/Book.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Library;
+
+class Book{
+    
+    function __construct($author, $publisher, $ISBN, $bookEdition) {
+        $this->author = $author;
+        $this->publisher = $publisher;
+        $this->ISBN = $ISBN;
+        $this->bookEdition = $bookEdition;
+    }
+    function getAuthor() {
+        return $this->author;
+    }
+
+    function getPublisher() {
+        return $this->publisher;
+    }
+
+    function getISBN() {
+        return $this->ISBN;
+    }
+
+    function getBookEdition() {
+        return $this->bookEdition;
+    }
+
+    function setAuthor($author) {
+        $this->author = $author;
+    }
+
+    function setPublisher($publisher) {
+        $this->publisher = $publisher;
+    }
+
+    function setISBN($ISBN) {
+        $this->ISBN = $ISBN;
+    }
+
+    function setBookEdition($bookEdition) {
+        $this->bookEdition = $bookEdition;
+    }   
+    
+          private $author;   
+          private $publisher;    
+          private $ISBN;              
+          private $bookEdition; 
+}
