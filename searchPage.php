76dd58621ff032cb5ec2e0133a1cc1ddd209d21a
Aurora del Rio <auroradelrio@gmail.com>
2020-04-30 16:10:04 +0100

    WebDesign for searching Page

---
+++ searchPage.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <title>Search Engine</title>
+        <link href="StyleSheetforForm.css" rel="stylesheet" type="text/css"/>
+      
+        
+    </head>
+        
+    <div style="margin: 20px; padding: 10px; ">
+    <div class="row">
+    <div class=" col-sm-12 text-center" style = "background-color:#FFCCCC; padding: 10px; font-family: 'Rubik', sans-serif;">        
+           <div class=" col-sm-12 text-center" style = "background-color: #EDEDED; font-family: 'Patua One', cursive;"> 
+           <h3> OurLibrary </h3>
+        </div> 
+        <div>
+        <p>Welcome to OurLibrary </p>       
+        <a style="position:absolute; top:0; right:0; margin-top: 60px; margin-right: 100px;" href="#" class="sign-out pull-right">
+    <span>Sign Out</span>   
+  </a>     
+</div>
+           </div>
+        
+        <div class="container-fluid bg-1 text-center" style = "background-color: #00BFB2; padding: 10px; margin: 50px;">
+            
+            <h3 class="h3">Search bar</h3>
+
+<div class="flexsearch">
+		<div class="flexsearch--wrapper">
+			<form class="flexsearch--form" action="#" method="post">
+				<div class="flexsearch--input-wrapper">
+					<input class="flexsearch--input" type="search" placeholder="search">
+				</div>
+				<input class="flexsearch--submit" type="submit" value="&#10140;"/>
+			</form>
+		</div>
+</div>
+        
+        </div>
+       
+                   <div class="container-fluid bg-2"  style = "background-color: #DCEDFF; padding: 200px; margin: 50px;">
+                 
+        </div> 
+    
+    </div>
+    </div>  
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
+
+
+
+
+
